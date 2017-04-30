635339a4e7bb6c15b00fef2b02a0ba371cbead6f
pmpontes <pedrompontes@outlook.com>
2017-05-01 00:06:14 +0100

    server implemented

--- src/data/Entry.java
+++ src/data/Entry.java
@@ -1,17 +1,41 @@
 package data;
 
-import java.util.Date;
+import org.json.simple.JSONObject;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "entry")
 public class Entry {
     private String date;
-    private int age;
-    private int weight;
-    private int cephalicPerimeter;
+    private String age;
+    private float weight;
+    private float cephalicPerimeter;
 
-    public Entry(String date, int age, int weight, int cephalicPerimeter) {
+    public Entry(String date, String age, float weight, float cephalicPerimeter) {
         this.date = date;
         this.age = age;
         this.weight = weight;
+        this.cephalicPerimeter = cephalicPerimeter;
+    }
+
+    @XmlElement
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @XmlElement
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @XmlElement
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    @XmlElement
+    public void setCephalicPerimeter(float cephalicPerimeter) {
         this.cephalicPerimeter = cephalicPerimeter;
     }
 
@@ -19,15 +43,24 @@ public class Entry {
         return date;
     }
 
-    public int getAge() {
+    public String getAge() {
         return age;
     }
 
-    public int getWeight() {
+    public float getWeight() {
         return weight;
     }
 
-    public int getCephalicPerimeter() {
+    public float getCephalicPerimeter() {
         return cephalicPerimeter;
+    }
+
+    public JSONObject toJSON() {
+        JSONObject entry = new JSONObject();
+        entry.put("date", date);
+        entry.put("age", age);
+        entry.put("weight", weight);
+        entry.put("cephalicPerimeter", cephalicPerimeter);
+        return entry;
     }
 }
--- src/data/Records.java
+++ src/data/Records.java
@@ -1,7 +1,14 @@
 package data;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 import java.util.ArrayList;
 
+@XmlRootElement(name = "records")
 public class Records {
     private String patientName;
     private ArrayList<Entry> recordTable;
@@ -15,6 +22,16 @@ public class Records {
         recordTable = new ArrayList<>();
     }
 
+    @XmlElement
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    @XmlElement
+    public void setRecordTable(ArrayList<Entry> recordTable) {
+        this.recordTable = recordTable;
+    }
+
     public boolean addRecordEntry(Entry newEntry){
         return recordTable.add(newEntry);
     }
@@ -22,5 +39,19 @@ public class Records {
     @Override
     public String toString() {
         return "Records for " + patientName + ":\n" + recordTable;
+    }
+
+    public JSONObject toJSON() {
+        JSONObject records = new JSONObject();
+        JSONArray recordList = new JSONArray();
+
+        for (Entry recordEntry : recordTable) {
+            recordList.add(recordEntry.toJSON());
+        }
+
+        records.put("patientName", patientName);
+        records.put("recordTable", recordList);
+
+        return records;
     }
 }
